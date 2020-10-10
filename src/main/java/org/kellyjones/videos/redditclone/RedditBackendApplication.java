c10be44b6f6e5d2051b71c5a8c1cbf9458d61de4
gearheadstu <bY425GSqzsk4>
2020-10-10 15:34:41 -0400

    Initial shell of app

---
+++ src/main/java/org/kellyjones/videos/redditclone/RedditBackendApplication.java
@@ -0,0 +1,13 @@
+package org.kellyjones.videos.redditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditBackendApplication.class, args);
+	}
+
+}
