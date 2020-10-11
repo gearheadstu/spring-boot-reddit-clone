e33e24736b520fa11e4e4973c8c070cc7ae00df3
gearheadstu <bY425GSqzsk4>
2020-10-11 16:10:17 -0400

    Added API info to Swagger config

--- src/main/java/org/kellyjones/videos/redditclone/configuration/SwaggerUIConfiguration.java
+++ src/main/java/org/kellyjones/videos/redditclone/configuration/SwaggerUIConfiguration.java
@@ -2,8 +2,10 @@ package org.kellyjones.videos.redditclone.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,15 @@ public class SwaggerUIConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.kellyjones"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Reddit Clone API")
+                .version("1.0")
+                .description("API for Spring Boot Reddit Clone")
                 .build();
     }
 }
