c278af3ae417eeeec2d81facbc213dde3486e527
gearheadstu <bY425GSqzsk4>
2020-10-11 16:03:04 -0400

    Added async suppport

--- src/main/java/org/kellyjones/videos/redditclone/RedditBackendApplication.java
+++ src/main/java/org/kellyjones/videos/redditclone/RedditBackendApplication.java
@@ -2,8 +2,10 @@ package org.kellyjones.videos.redditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RedditBackendApplication {
 
 	public static void main(String[] args) {
