6928b3012663a15a57d2852af559cd150863fe89
gearheadstu <bY425GSqzsk4>
2020-10-12 12:14:05 -0400

    Added Subreddit API

---
+++ src/main/java/org/kellyjones/videos/redditclone/mapper/SubredditMapper.java
@@ -0,0 +1,25 @@
+package org.kellyjones.videos.redditclone.mapper;
+
+import org.kellyjones.videos.redditclone.dto.SubredditDto;
+import org.kellyjones.videos.redditclone.model.Post;
+import org.kellyjones.videos.redditclone.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
